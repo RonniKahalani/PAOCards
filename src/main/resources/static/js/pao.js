389ade2a568b7825dc6c20c77cba85ca63335dee
Ronni Kahalani <ronni.kahalani@gmail.com>
2024-04-04 23:09:33 +0200

    Added PAO template

--- src/main/resources/static/js/pao.js
+++ src/main/resources/static/js/pao.js
@@ -1,6 +1,12 @@
 "use strict";
 
 async function loadMatrix(id) {
+
+    const template = localStorage.getItem("pao-template");
+    if(template) {
+        document.getElementById("content").innerHTML = template;
+    }
+
     const response = await fetch("http://localhost:8080/api/v1/pao/" + id);
     if(response.ok) {
         const matrix = await response.json();
@@ -10,3 +16,15 @@ async function loadMatrix(id) {
 
 }
 
+/**
+ * Load template from file.
+ */
+var fileURL = "pao-template.html";
+
+fetch(`${fileURL}`).then(function(response) {
+    response.text().then(function(text) {
+        console.log(text); // output the file content as text
+        // put data in localStorage or sessionStorage here
+        localStorage.setItem("pao-template", text);
+    })
+});
