b749bbdd57b08e41e31374dced984aeaf201bda7
Ronni Kahalani <ronni.kahalani@gmail.com>
2024-04-12 21:22:38 +0200

    added !==

--- src/main/resources/static/js/pao.js
+++ src/main/resources/static/js/pao.js
@@ -346,7 +346,7 @@ function shuffle(array) {
     let currentIndex = array.length;
 
     // While there remain elements to shuffle...
-    while (currentIndex != 0) {
+    while (currentIndex !== 0) {
 
         // Pick a remaining element...
         let randomIndex = Math.floor(Math.random() * currentIndex);
