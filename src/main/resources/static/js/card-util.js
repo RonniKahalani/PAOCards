eeacd205f5db8d20c688d3c6e048c5a7f14b013c
Ronni Kahalani <ronni.kahalani@gmail.com>
2024-04-25 23:01:19 +0200

    Documentation

--- src/main/resources/static/js/card-util.js
+++ src/main/resources/static/js/card-util.js
@@ -1,11 +1,25 @@
 "use strict";
 
+/**
+ * Card utility class.
+ */
 export class CardUtil {
 
+    /**
+     * Returns a SVG card image.
+     * @param cardId
+     * @param suit
+     * @returns {string}
+     */
     getSVGCardImageUrl(cardId, suit) {
         return "svg/cards/" + cardId.toLowerCase() + "_of_" + suit.toLowerCase() + ".svg";
     }
 
+    /**
+     * Returns a textual id for a given card. Ex. Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen or King.
+     * @param card
+     * @returns {*|string}
+     */
     getCardId(card) {
         let value = card.value;
         switch (true) {
